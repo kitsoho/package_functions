3d8b90e90ecc6aab79e55d06d84e8c1e8fb0fe2c
Hollie Kitson <hkitson@zevross.com>
2017-05-30 16:14:06 -0400

    Fixed geography-specific functions, added more error-handling

--- census_related/census_data_to_postgres2.R
+++ census_related/census_data_to_postgres2.R
@@ -46,7 +46,6 @@ get_connection()
 
 
 
-
 #' Title
 #'
 #' @param schema name of schema within db
@@ -58,11 +57,9 @@ get_connection()
 
 
 
-acs_pg_check_table<-function(schema = "acs_census_tabular", reload = FALSE){
+acs_pg_check_table<-function(schema = "acs_census_tabular", tablename = tablename, reload = FALSE){
 	
 	make_schema_public(schema)
-	
-	tablename <<- paste0("acs_", year, "_", geolevel, "_", span, "yr_", table)
 
 	tableexists <- dplyr::db_has_table(.connection$con, tablename)
 	
@@ -77,9 +74,11 @@ acs_pg_check_table<-function(schema = "acs_census_tabular", reload = FALSE){
 
 
 
-acs_send_data_to_postgres<-function(){
+acs_send_data_to_postgres<-function(year = NULL, geolevel = NULL, span = NULL, table = NULL){
 	
-	acs_pg_check_table(reload = TRUE)
+	tablename <- paste0("acs_", year, "_", geolevel, "_", span, "yr_", table)
+	
+	acs_pg_check_table(tablename = tablename)
 	
 	# dat<-acs_get_data(year = 2015, geolevel = "county", table = "B17010", span = 5)
 	dat<-acs_get_data(tablename)
@@ -90,7 +89,7 @@ acs_send_data_to_postgres<-function(){
 }
 
 
-acs_send_data_to_postgres()
+acs_send_data_to_postgres(year = 2010, geolevel = "tract", span = 5, table = "B17010")
 
 
 
@@ -107,11 +106,14 @@ acs_send_data_to_postgres()
 #' acs_get_data(year = 2015, geolevel = "state", table = "B17010", span = 5)
 
 
-acs_get_data<-function(year = 2015, geolevel = "state", table = "B17010", span = 5){
+acs_get_data<-function(tablename = tablename){
+	
+	year <- as.numeric(sapply(strsplit(tablename, "_"), "[", 2))
+	geolevel <- sapply(strsplit(tablename, "_"), "[", 3)
+	span <- as.numeric(gsub("yr", "", sapply(strsplit(tablename, "_"), "[", 4)))
+	table <- sapply(strsplit(tablename, "_"), "[", 5)
+	
 
-    # pgtable <<- paste0("acs_", year, "_", geolevel, "_", span, "yr_", table)
-      
-      
     datstate <- function(){
     	
     	acsgeo<-geo.make(state = "*")
@@ -252,74 +254,25 @@ acs_get_data<-function(year = 2015, geolevel = "state", table = "B17010", span =
 	
 
 
-dat<-acs_get_data(year = 2015, geolevel = "state", table = "B17010", span = 5)
+# dat<-acs_get_data(year = 2015, geolevel = "state", table = "B17010", span = 5)
+
+
+
+
+
+
+
+acs_create_table_varnames<-function(){
+	
+	acsgeo<-geo.make(state = "*")
+	acsdat<-acs.fetch(endyear = year, span = span, geography = acsgeo,
+		table.number = table, col.names = "auto")
+	
+}
 
 
 
 	
-
-
-
-
-acs_add_data_to_postgres <- function(filedir, 
-        dbname = "testing", 
-        schema = "census_geography", 
-        reload = FALSE){
-        
-        
-
-        con <- .connection$con
-        make_schema_public(schema)
-        
-        tablename <- paste0("acs_", year, "_", geolevel, "_", span, "yr_", table)
-        
-                
-        # basename <- gsub(".shp", "", basename(filedir), fixed = TRUE)
-        schema_table <- paste(schema, basename, sep = ".")
-        tableexists <- dplyr::db_has_table(.connection$con, tablename)
-        
-        
-        if(tableexists & !reload) stop(paste("Table", basename, "already exists in the database"))
-        
-        if(tableexists & reload){
-                dplyr::db_drop_table(con, basename)
-        }
-        
-        
-        RPostgreSQL::dbWriteTable(con, tablename, dat, row.names = FALSE)
-        
-        
-        # cmd <- sprintf('ogr2ogr -f "PostgreSQL" PG:"host=192.168.1.4 port=5432 user=postgres dbname=%s password=spatial" "%s" -nln "%s.%s" -nlt PROMOTE_TO_MULTI -lco "GEOM_TYPE=geometry" -lco "GEOMETRY_NAME=geom"',
-        #         dbname, filedir, schema, basename)
-        # 
-        # cat("About to add to database using command\n", cmd)
-        # 
-        # system(cmd)
-        # invisible()
-        
-}
-
-
-
-
-
-
-
-
-
-add_data_to_postgres <- function(){
-}
-        
-        
-
-tableexists <- dplyr::db_has_table(.connection$con, "hollie333")
-        
-if(tableexists & !reload) stop(paste("Table", basename, "already exists in the database"))
-
-if(tableexists & reload){
-        dplyr::db_drop_table(con, basename)
-}
-
 
 
 
@@ -334,48 +287,10 @@ make_schema_public <- function(schemaname){
 
 
 
-make_schema_public("temp333")
 
-
-	
-	var_description<-function(table){
-		
-		dat_auto<-acs.fetch(endyear = year, span = span,
-			geography = geo.make(state = "*"),
-			table.number = table, col.names = "auto")
-		
-		dat_pretty<-acs.fetch(endyear = year, span = span, 
-			geography = geo.make(state = "*"),
-			table.number = table, col.names = "pretty")
-		
-		dat<-data.frame(name = table, 
-			id = dat_auto@acs.colnames,
-			desc = dat_pretty@acs.colnames)
-		
-		return(dat)
-	}
-
-	
 		
 	
 	
-	
-	
-# 	dat<-acs.fetch(endyear = year, span = 5, geography = geo,
-# 		table.number = table, col.names = "auto")
-# 	
-# 	
-# 	datgeoid<-switch(type,
-# 		state = str_pad(dat@geography$state, 2, "left", pad="0"),
-# 		county = paste0(str_pad(dat@geography$state, 2, "left", pad="0"), 
-# 			str_pad(dat@geography$county, 3, "left", pad="0")),
-# 		tract = paste0(str_pad(dat@geography$state, 2, "left", pad="0"), 
-# 			str_pad(dat@geography$county, 3, "left", pad="0"), 
-# 			str_pad(dat@geography$tract, 6, "left", pad="0")),
-# 		blockgroup = 
-# 	)
-# 	
-# }
 
 
 
@@ -385,130 +300,3 @@ make_schema_public("temp333")
 
 
 
-
-
-
-
-download_tiger_zip <- function(year = year, type = type, directory = NULL) {
-	# year <- "2016"
-	# type <- "CD"
-	url1 <- sprintf("https://www2.census.gov/geo/tiger/TIGER%s/%s/", year, type)
-	cat("Downloading from ", url1)
-	zips_avail <- find_zips_at_URL(url1)
-	
-	if(length(zips_avail)>1) stop("There is more than one zip file")
-	
-	url_to_file <- paste0(url1, zips_avail)
-	
-	directory <- ifelse(is.null(directory), tempdir(), directory)
-	directory <- gsub("//", "/", directory)
-	
-	filedir <- paste0(directory, "/", zips_avail)
-	download.file(url_to_file, filedir, mode = 'wb')
-	unzip(filedir, exdir = directory)
-	shape <- gsub(".zip", ".shp", filedir)
-	
-	cat("File written to:", filedir)
-	
-	shape
-	
-}
-
-
-
-#' Find hrefs that have ".zip" in them
-#'
-#' @param url A url
-#' @return A list of hrefs that have zips.
-#' @export
-#' @details
-#' This function will find and return the href tags that have a '.zip' in them
-#' @examples
-#' add(1, 1)
-#' add(10, 1)
-
-find_zips_at_URL <- function(url1){
-	
-	website <- read_html(url1)
-	hrefs <- website %>%
-		html_nodes('a') %>%
-		html_attr('href')
-	
-	hrefs[grepl(".zip", hrefs, fixed = TRUE)]
-	
-}
-
-
-
-#' Take input file path and upload to postgres using ogr2ogr in background
-#'
-#' @param filedir path to a shapefile
-#' @param reload If an existing table exists should it be dropped
-#' @return A list of hrefs that have zips.
-#' @export
-#' @examples
-#' add_geo_to_postgres(path, reload = TRUE)
-#'
-
-add_geo_to_postgres <- function(filedir, dbname = "usa_data", schema = "census_geography", reload = FALSE){
-	# filedir <- shape
-	#DROP TABLE "census_geography"."tl_2016_us_state";
-	con <- .connection$con
-	make_schema_public(schema)
-	
-	basename <- gsub(".shp", "", basename(filedir), fixed = TRUE)
-	schema_table <- paste(schema, basename, sep = ".")
-	tableexists <- dplyr::db_has_table(.connection$con, basename)
-	
-	
-	if(tableexists & !reload) stop(paste("Table", basename, "already exists in the database"))
-	
-	if(tableexists & reload){
-		dplyr::db_drop_table(con, basename)
-	}
-	
-	cmd <- sprintf('ogr2ogr -f "PostgreSQL" PG:"host=192.168.1.4 port=5432 user=postgres dbname=%s password=spatial" "%s" -nln "%s.%s" -nlt PROMOTE_TO_MULTI -lco "GEOM_TYPE=geometry" -lco "GEOMETRY_NAME=geom"',
-		dbname, filedir, schema, basename)
-	
-	cat("About to add to database using command\n", cmd)
-	
-	system(cmd)
-	invisible()
-	
-}
-
-
-
-#' Run SQL to make a schema part of public so no dot syntax is required
-#'
-#' @param url A url
-#' @return A list of hrefs that have zips.
-#' @export
-#' @examples
-#'
-#'
-
-make_schema_public <- function(schemaname){
-	
-	RPostgreSQL::dbSendQuery(.connection$con, sprintf("SET search_path = '%s', public", schemaname))
-	invisible()
-}
-
-
-
-#' Find hrefs that have ".zip" in them
-#'
-#' @param url A url
-#' @return A list of hrefs that have zips.
-#' @export
-#' @examples
-#' get_table_insert_pg(2016, "CD")
-
-
-get_table_insert_pg <- function(year, type, reload = FALSE){
-	
-	path <- download_tiger_zip(year, type)
-	add_geo_to_postgres(path, reload = reload)
-	
-	
-}
